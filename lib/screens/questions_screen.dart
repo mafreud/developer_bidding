c0b1b9d41b0cd98af6164b0fbbc1cce27a66d470
Masahiro Aoki <rafe.kun@gmail.com>
2019-05-18 23:56:37 +0900

    change input box message

--- lib/screens/questions_screen.dart
+++ lib/screens/questions_screen.dart
@@ -5,14 +5,12 @@ import 'package:easy_fund/data.dart';
 import 'package:easy_fund/components/reusable_card.dart';
 
 class QuestionsScreen extends StatefulWidget {
-
   static String id = 'questions_screen';
   @override
   _QuestionsScreenState createState() => _QuestionsScreenState();
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
-
   double gpa = 3.0;
 
   @override
@@ -20,7 +18,10 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
     return Scaffold(
       appBar: AppBar(
         leading: null,
-        title: Text('基本情報', style: kBoldTextStyle,),
+        title: Text(
+          '基本情報',
+          style: kBoldTextStyle,
+        ),
         backgroundColor: Colors.lightBlueAccent,
       ),
       backgroundColor: Colors.white,
@@ -30,89 +31,82 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              '姓', style: TextStyle(color: Colors.black),
-              textAlign: TextAlign.left,
+            TextField(
+              onChanged: (value) {
+                //Do something with the user input.
+              },
+              decoration: kTextFieldDecoration.copyWith(hintText: 'Last Name'),
+            ),
+            SizedBox(
+              height: 13.0,
             ),
             TextField(
               onChanged: (value) {
                 //Do something with the user input.
               },
-              decoration: kTextFieldDecoration.copyWith(hintText: ''),
-            ),
-
-            Text(
-              '名', style: TextStyle(color: Colors.black),
-              textAlign: TextAlign.left,
-            ),
-            TextField(
-              onChanged: (value) {
-                //Do something with the user input.
-              },
-              decoration: kTextFieldDecoration.copyWith(hintText: ''),
+              decoration: kTextFieldDecoration.copyWith(hintText: 'First Name'),
             ),
             Text(
-              'GPA', style: TextStyle(color: Colors.black),
+              'GPA',
+              style: TextStyle(color: Colors.black),
               textAlign: TextAlign.left,
             ),
-          SliderTheme(
-              data: SliderTheme.of(context).copyWith(
-                  inactiveTrackColor: Colors.grey,
-                  activeTrackColor: Colors.pinkAccent,
-                  thumbColor: Color(0xFFEB1555),
-                  overlayColor: Color(0x29EB1555),
-                  thumbShape:
-                  RoundSliderThumbShape(enabledThumbRadius: 15.0),
-                  overlayShape:
-                  RoundSliderOverlayShape(overlayRadius: 30.0)),
-              child: Slider(
-                  value: gpa,
-                  min: 2.0,
-                  max: 4.0,
-                  onChanged: (double newValue) {
-                    setState(() {
-                      gpa = newValue;
-                      print(gpa);
-                    });
-                  })),
-            DropdownButton(items: majors
-                    .map<DropdownMenuItem<String>>((String value) {
-      return DropdownMenuItem<String>(
-      value: value,
-      child: Text(value),
-      );
-      })
-          .toList(), onChanged: (String newValueSelected){
-                setState(() {
-                  print(newValueSelected);
-                });
-            }),
-
-
+            SliderTheme(
+                data: SliderTheme.of(context).copyWith(
+                    inactiveTrackColor: Colors.grey,
+                    activeTrackColor: Colors.pinkAccent,
+                    thumbColor: Color(0xFFEB1555),
+                    overlayColor: Color(0x29EB1555),
+                    thumbShape: RoundSliderThumbShape(enabledThumbRadius: 15.0),
+                    overlayShape: RoundSliderOverlayShape(overlayRadius: 30.0)),
+                child: Slider(
+                    value: gpa,
+                    min: 2.0,
+                    max: 4.0,
+                    onChanged: (double newValue) {
+                      setState(() {
+                        gpa = newValue;
+                        print(gpa);
+                      });
+                    })),
+            DropdownButton(
+                items: majors.map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                onChanged: (String newValueSelected) {
+                  setState(() {
+                    print(newValueSelected);
+                  });
+                }),
             Text("性別"),
             ReusableCard(
-              colour: Colors.lightBlueAccent,
-              cardChild: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
+                colour: Colors.lightBlueAccent,
+                cardChild: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
                     "男性",
-                    textAlign: TextAlign.center,),
-              )),
+                    textAlign: TextAlign.center,
+                  ),
+                )),
             ReusableCard(
                 colour: Colors.lightBlueAccent,
                 cardChild: Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: Text(
                     "女性",
-                    textAlign: TextAlign.center,),
+                    textAlign: TextAlign.center,
+                  ),
                 )),
-            RoundedButton(buttonText: "奨学金を見つける", buttonColor: Colors.lightBlueAccent, buttonPressed: (){
-            }),
+            RoundedButton(
+                buttonText: "奨学金を見つける",
+                buttonColor: Colors.lightBlueAccent,
+                buttonPressed: () {}),
           ],
         ),
       ),
     );
   }
 }
-
-
