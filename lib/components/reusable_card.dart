1f7247cbe07e374070246fa6ce6d0415e087ce8b
Your Name <you@example.com>
2019-05-25 20:44:07 +0900

    push用２

--- lib/components/reusable_card.dart
+++ lib/components/reusable_card.dart
@@ -1,3 +1,6 @@
+
+
+
 import 'package:flutter/material.dart';
 
 class ReusableCard extends StatelessWidget {
--- lib/components/scholarship_card.dart
+++ lib/components/scholarship_card.dart
@@ -1,37 +1,63 @@
 import 'package:flutter/material.dart';
 import 'colors.dart';
+import 'package:easy_fund/screens/scholarship_detail_screen.dart';
 
 class ScholarshipCard extends StatelessWidget {
   ScholarshipCard({this.budged, this.deadline, this.scholarshipName});
 
   String scholarshipName;
-  DateTime deadline;
+  dynamic deadline;
   int budged;
+
+  void CardTapped(context){
+    Navigator.push(context,
+      MaterialPageRoute(builder: (context) => ScholarshipDetail(scholarshipName:this.scholarshipName)),);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Row(
-        children: <Widget>[
-          SizedBox(
-            width: 10.0,
-          ),
-          Icon(Icons.check_circle, color: easyFundMainColor, size: 20.0),
-          SizedBox(
-            width: 10.0,
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text('業務スーパー奨学金'),
-              Text('申し込み期限：09/18/2019'),
-              Text('提供額:400万')
-            ],
-          ),
-        ],
+    return GestureDetector(
+      onTap: (){
+        Navigator.push(context, MaterialPageRoute(builder: (context) => ScholarshipDetail(scholarshipName: scholarshipName,)));
+      },
+      child: Card(
+        child: Row(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(27.0),
+              child:
+              Icon(Icons.check_circle, color: easyFundMainColor, size: 20.0),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 15.0, bottom: 15.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    this.scholarshipName,
+                    style: TextStyle(fontSize: 13.0, fontWeight: FontWeight.bold),
+                  ),
+                  Text(this.budged.toString()),
+//                  Text(this.deadline,
+//                    style: TextStyle(
+//                        color: Colors.red,
+//                        fontSize: 11.0,
+//                        fontWeight: FontWeight.bold),
+//                  ),
+                ],
+              ),
+            ),
+            SizedBox(width: 60.0),
+            Icon(
+              Icons.keyboard_arrow_right,
+              color: easyFundMainColor,
+              size: 30.0,
+            ),
+          ],
+        ),
+        elevation: 5.0,
+        color: Colors.white,
       ),
-      elevation: 5.0,
-      color: Colors.white,
     );
   }
 }
